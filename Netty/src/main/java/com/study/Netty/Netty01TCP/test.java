25e5def58b55bfeded819ea3c4ed538c6c975807
WeiZY126 <1369150463@qq.com>
2022-06-29 23:22:04 +0800

    2022/6/29

--- Netty/src/main/java/com/study/Netty/Netty01TCP/test.java
+++ Netty/src/main/java/com/study/Netty/Netty01TCP/test.java
@@ -4,6 +4,9 @@ import io.netty.util.NettyRuntime;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(NettyRuntime.availableProcessors());
+//        System.out.println(NettyRuntime.availableProcessors());
+        String a = "aaa";
+        String b = a+"";
+        System.out.println(a==b);
     }
 }
---
+++ Netty/src/main/java/com/study/Netty/Netty02HTTP/TestServerInitializer.java
@@ -0,0 +1,23 @@
+package com.study.Netty.Netty02HTTP;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        //向管道加入处理器
+
+        //得到管道
+        ChannelPipeline pipeline = ch.pipeline();
+
+        //加入一个netty提供的httpServerCodec codec=>[coder-decoder]
+        //HttpServerCodec说明
+        //1.HttpServerCodec是netty提供的处理http的编解码器
+        pipeline.addLast("MyHttpServerCodec",new HttpServerCodec());
+        //2.增加一个自定义的Handler
+        pipeline.addLast("MyTestHttpServerHandler",new TestHttpServerHandler());
+    }
+}
