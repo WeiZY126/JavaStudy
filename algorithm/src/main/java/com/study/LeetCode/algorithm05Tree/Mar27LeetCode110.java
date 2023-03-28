7d1b5bfe97e456e643441b29930d7d0f73e64db2
WeiZY126 <1369150463@qq.com>
2023-03-28 23:35:57 +0800

    2023/03/28

--- algorithm/src/main/java/com/study/LeetCode/algorithm05Tree/Mar27LeetCode110.java
+++ algorithm/src/main/java/com/study/LeetCode/algorithm05Tree/Mar27LeetCode110.java
@@ -4,4 +4,21 @@ package com.study.LeetCode.algorithm05Tree;
  * 平衡二叉树*
  */
 public class Mar27LeetCode110 {
+    public boolean LeetCode110(TreeNode root) {
+        if (root == null)
+            return true;
+        if (Recursion(root) == -1)
+            return false;
+        return true;
+    }
+
+    public static int Recursion(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = Recursion(root.left);
+        int right = Recursion(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1)
+            return -1;
+        return Math.max(left, right) + 1;
+    }
 }
---
+++ algorithm/src/main/java/com/study/LeetCode/algorithm05Tree/Mar28LeetCode226.java
@@ -0,0 +1,17 @@
+package com.study.LeetCode.algorithm05Tree;
+
+/**
+ * 反转二叉树*
+ */
+public class Mar28LeetCode226 {
+    public TreeNode LeetCode226Recursion(TreeNode root) {
+        if (root == null)
+            return root;
+        LeetCode226Recursion(root.left);
+        LeetCode226Recursion(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
