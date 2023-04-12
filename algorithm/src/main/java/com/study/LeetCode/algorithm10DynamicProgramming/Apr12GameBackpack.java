9a86e20d5951379f8f11287bf3e8791ef3e2e098
WeiZY126 <1369150463@qq.com>
2023-04-12 23:50:43 +0800

    2023/04/12

--- algorithm/src/main/java/com/study/LeetCode/algorithm10DynamicProgramming/Apr12GameBackpack.java
+++ algorithm/src/main/java/com/study/LeetCode/algorithm10DynamicProgramming/Apr12GameBackpack.java
@@ -10,7 +10,7 @@ import java.util.Set;
  */
 public class Apr12GameBackpack {
     public static void main(String[] args) {
-        Prop[] props = {new Prop("xxj", 1500, 1), new Prop("xxj2", 2000, 3), new Prop("xxj3", 3000, 4)};
+        Prop[] props = {new Prop("xxj", 1500, 1), new Prop("xxj2", 2000, 3), new Prop("xxj3", 4000, 4)};
         outBag(props, 4);
 
     }
@@ -20,7 +20,7 @@ public class Apr12GameBackpack {
         for (int i = 0; i < props.length; i++) {
             Prop prop = props[i];
             for (int j = 0; j < bagSize; j++) {
-                int thisBagSize = j+1;
+                int thisBagSize = j + 1;
                 if (i == 0) {
                     if (prop.cost > j + 1)
                         resArray[i][j] = new PropsList(0);
@@ -33,19 +33,26 @@ public class Apr12GameBackpack {
                 else {
                     Integer lastValue = resArray[i - 1][j].value;
                     int thisValue;
-                    if (prop.cost - thisBagSize <= 0) {
+                    if (prop.cost == thisBagSize) {
                         thisValue = prop.value;
-                    } else
-                        thisValue = prop.value + resArray[i][prop.cost - thisBagSize].value;
-                    if (thisValue > lastValue) {
-                        PropsList propsList = new PropsList(thisValue).addProps(prop);
-                        for (Prop prop1 : resArray[i - 1][j].props) {
-                            propsList.addProps(prop1);
+                        if (thisValue > lastValue) {
+                            PropsList propsList = new PropsList(thisValue).addProps(prop);
+                            resArray[i][j] = propsList;
+                            continue;
                         }
-                        resArray[i][j] = propsList;
                     } else {
-                        resArray[i][j] = resArray[i - 1][j];
+                        thisValue = prop.value + resArray[i - 1][thisBagSize - prop.cost].value;
+                        if (thisValue > lastValue) {
+                            PropsList propsList = new PropsList(thisValue).addProps(prop);
+                            for (Prop prop1 : resArray[i - 1][thisBagSize - prop.cost].props) {
+                                propsList.addProps(prop1);
+                            }
+                            resArray[i][j] = propsList;
+                            continue;
+                        }
                     }
+                    resArray[i][j] = resArray[i - 1][j];
+
                 }
             }
         }
